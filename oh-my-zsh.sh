adf8c85a8367c2f4f7cad376dbf06a0ef79cb26d
Benjamin Boudreau <boudreau.benjamin@gmail.com>
2011-05-24 08:49:39 -0400

    Fix compdef undefined warning

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -3,6 +3,10 @@
 # add a function path
 fpath=($ZSH/functions $fpath)
 
+# Load and run compinit
+autoload -U compinit
+compinit -i
+
 # Load all of the config files in ~/oh-my-zsh that end in .zsh
 # TIP: Add files you don't want in git to .gitignore
 for config_file ($ZSH/lib/*.zsh) source $config_file
@@ -10,10 +14,6 @@ for config_file ($ZSH/lib/*.zsh) source $config_file
 # Add all defined plugins to fpath
 plugin=${plugin:=()}
 for plugin ($plugins) fpath=($ZSH/plugins/$plugin $fpath)
-
-# Load and run compinit
-autoload -U compinit
-compinit -i
 
 # Load all of the plugins that were defined in ~/.zshrc
 for plugin ($plugins); do
