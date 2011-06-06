3940f9e7e52e786441d9b4676bfc4f97e0db3dcb
Benjamin Boudreau <boudreau.benjamin@gmail.com>
2011-06-06 13:17:36 -0400

    Add todo item for remark plugin - on error jobs aren't getting killed

--- plugins/remark/remark.plugin.zsh
+++ plugins/remark/remark.plugin.zsh
@@ -4,13 +4,18 @@
 #
 # Usage: logview $ZSH/plugins/remark/examples/syslog /var/log/syslog
 
-
+# TODO Improve to kill jobs when remark find an error in the files
 function logview() {
   templogfile=`mktemp`
 
   tail -n +0 --follow=name "$2" | remark "$1" > $templogfile &
   pidRemark=$!
 
+  #if [ $? -ne 0 ]; then
+  #  kill $pidRemark &> /dev/null
+  #  exit 1
+  #fi
+
   less -R +F $templogfile
-  kill $pidRemark
+  kill $pidRemark &> /dev/null
 }
